04146ea0b54353c9ad7cc2551fed9c32be49cafb
Petteri <petteri.koivuniemi@live.fi>
2021-02-26 11:23:37 +0200

    Added POST validation to locations

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const { post } = require("./routes/locations.js");
 let locationsRouter = require("./routes/locations.js");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+
+const validateLocation = (loc) => {
+  let valid = true;
+  if (!("latitude" in loc)) valid = false;
+  else if (typeof loc.latitude != "number") valid = false;
+  else if (loc.latitude > 90 || loc.latitude < -90) valid = false;
+
+  if (!("longitude" in loc)) valid = false;
+  else if (typeof loc.longitude != "number") valid = false;
+  else if (loc.longitude > 180 || loc.longitude < -180) valid = false;
+
+  return valid;
+};
+
+app.use("/api/locations/", (req, res, next) => {
+  console.log(req.method);
+  if (req.method === "POST") {
+    if (validateLocation(req.body)) next();
+    else res.send(400).end();
+  } else next();
+});
+
 app.use("/api/locations", locationsRouter);
 app.use(express.static("public"));
 
